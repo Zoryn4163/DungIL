37f8aa4469ca93804a308ca030f21d9a43bb39e4
Zoryn Aaron <zoryn4163@gmail.com>
2018-03-03 17:22:41 -0500

    code cleanup

--- DungILTestMod/TestMod.cs
+++ DungILTestMod/TestMod.cs
@@ -8,30 +8,6 @@ namespace DungILTestMod
         public override void Init()
         {
             Log.Out("We loaded a test mod!");
-            test();
-            test2();
-            test3();
-            test4();
-        }
-
-        public string test()
-        {
-            return "test";
-        }
-
-        public string test2()
-        {
-            return test();
-        }
-
-        public string test3()
-        {
-            return test() + test2();
-        }
-
-        public string test4()
-        {
-            return test() + test2() + "test";
         }
     }
 }
